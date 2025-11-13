9c81760ed04db75d903b1901f2ea7625fa4243af
Doman <nanodoman@gmail.com>
2025-11-13 16:05:03 +0100

    refaktor(game002): v2
    Squashed commit of the following:
    
    commit 262b27d1ed8461d0d5364edcf36db9c4d86e9e27
    Author: Doman <nanodoman@gmail.com>
    Date:   Thu Nov 13 16:02:59 2025 +0100
    
        style(game002): clear comments
    
    commit c570c3ec0d6e6e7f646215b41af74fa989b73f5e
    Author: Doman <nanodoman@gmail.com>
    Date:   Thu Nov 13 16:00:24 2025 +0100
    
        style(game002): icons and settings menu
    
    commit 6acddc27d1adc9b7d8f42a295067a385ed67e1ee
    Author: Doman <nanodoman@gmail.com>
    Date:   Thu Nov 13 15:29:34 2025 +0100
    
        fix(game002): rerandomize unsolvable setups
    
    commit 2dd9d1bfc992b54fd169a8e6ef2c20136659054f
    Author: Doman <nanodoman@gmail.com>
    Date:   Thu Nov 13 15:10:59 2025 +0100
    
        style(game002): update width
    
    commit 1909ef1b6e065a92c8555aa93433b77a24ca7572
    Author: Doman <nanodoman@gmail.com>
    Date:   Thu Nov 13 13:38:06 2025 +0100
    
        doc(game002): screenshots
    
    commit 11b60386d5f5608c0c9770ffa1b566adee04d078
    Author: Doman <nanodoman@gmail.com>
    Date:   Thu Nov 13 13:06:04 2025 +0100
    
        refactor(game002): dynamic check function
    
    commit f62cbeeebc99e95a5ecac24042255a89f8f9cea6
    Author: Doman <nanodoman@gmail.com>
    Date:   Thu Nov 13 11:17:17 2025 +0100
    
        refactor(game002): structure
    
    commit e9eaa2062b321050a6febf2a9cd33c872a9040ee
    Author: Doman <nanodoman@gmail.com>
    Date:   Thu Nov 13 11:03:25 2025 +0100
    
        feat(game002): tile mapping based on game size + fixes
    
    commit 22fefaf1bb77f5e2d099822ad5cfa4fa3963fae6
    Author: Doman <nanodoman@gmail.com>
    Date:   Thu Nov 13 01:21:08 2025 +0100
    
        fix(game002): button text
    
    commit c18223d5cdf31bb151989b0dd584c4e0900979c0
    Author: Doman <nanodoman@gmail.com>
    Date:   Thu Nov 13 01:19:59 2025 +0100
    
        feat(game002): add settings
    
    commit 337b43637016fae256786ceb09419a468059bb5f
    Author: Doman <nanodoman@gmail.com>
    Date:   Wed Nov 12 23:46:45 2025 +0100
    
        refactor(game002): all wip

---
+++ game002-slizzle/doc/slizzle.md
@@ -0,0 +1,4 @@
+# Slizzle
+
+[<img src="Slizzle1.png" width="400"/>](Slizzle1.png)
+[<img src="Slizzle2.png" width="400"/>](Slizzle2.png)
---
+++ game002-slizzle/src/main.js
@@ -0,0 +1,166 @@
+class Game {
+  #size = 3;
+  #sizeOptions = Object.freeze([3, 4, 5]);
+  #playing = false;
+  #moves = 0;
+  #time = 0;
+  #timer;
+  #tick = 1000;
+  #emptyTile = undefined;
+
+  static isAdjacentTile(tile1, tile2) {
+    if (tile2.row === tile1.row) {
+      return Math.abs(tile2.col - tile1.col) === 1;
+    }
+
+    if (tile2.col === tile1.col) {
+      return Math.abs(tile2.row - tile1.row) === 1;
+    }
+
+    return false;
+  }
+
+  constructor() {
+    this.options = document.querySelector('#options');
+    this.game = document.querySelector('#game');
+    this.moveCount = document.querySelector('#move-count');
+    this.startButton = document.querySelector('#start-button');
+    this.timeDisplay = document.querySelector('#time-display');
+    this.setupTiles();
+  }
+
+  initialize() {
+    this.#playing = true;
+    this.resetGame();
+    this.randomize();
+    this.#timer = setInterval(this.#incrementTime.bind(this), this.#tick);
+    const staticTile = document.querySelector('.tile.static');
+    staticTile.style.visibility = 'hidden';
+    this.#emptyTile = {
+      row: this.#size,
+      col: this.#size,
+    };
+    this.startButton.innerText = 'Restart';
+  }
+
+  changeGrid(size) {
+    this.resetGame();
+    this.setupTiles(size);
+  }
+
+  setupTiles(size = 3) {
+    if (this.#sizeOptions.includes(+size)) {
+      this.#size = +size;
+      this.game.replaceChildren();
+      document.documentElement.style.setProperty('--size', this.#size);
+
+      for (let index = 0; index < this.#size ** 2; index++) {
+        const tileNumber = index + 1;
+        const tile = document.createElement('div');
+        const row = Math.floor(index / this.#size) + 1;
+        const col = (index % this.#size) + 1;
+        const last = tileNumber === this.#size ** 2;
+        tile.id = `t${tileNumber}`;
+        tile.classList.value = `tile row-${row} col-${col}`;
+        if (last) tile.classList.add('static');
+        tile.innerText = tileNumber;
+
+        this.game.insertAdjacentElement('beforeEnd', tile);
+      }
+    }
+  }
+
+  resetGame() {
+    this.#moves = 0;
+    this.#time = 0;
+    this.moveCount.value = this.#moves;
+    this.timeDisplay.innerText = '00:00';
+    this.startButton.innerText = 'Start';
+    clearInterval(this.#timer);
+  }
+
+  randomize() {
+    const tiles = [...document.querySelectorAll('.tile:not(.static)')];
+    let iteration = 0;
+    const permutation = [];
+
+    while (tiles.length) {
+      const index = Math.round(Math.random() * 10) % tiles.length;
+      const row = (iteration % this.#size) + 1;
+      const col = (Math.ceil(iteration / this.#size) % (row === this.#size ? this.#size - 1 : this.#size)) + 1;
+      permutation.push(tiles[index].id);
+      tiles[index].classList.value = `tile row-${row} col-${col}`;
+      tiles.splice(index, 1);
+      iteration++;
+    }
+
+    const nums = permutation.map((t) => parseInt(t.slice(1), 10));
+    let inversions = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+      for (let j = i + 1; j < nums.length; j++) {
+        if (nums[i] > nums[j]) {
+          inversions++;
+        }
+      }
+    }
+
+    console.log(inversions % 2);
+
+    if (inversions % 2 !== 0) this.randomize();
+  }
+
+  check() {
+    const tiles = document.querySelectorAll('.tile:not(.static)');
+    const allCorrect = Array.from(tiles).every((tile, index) => {
+      const [row, col] = index
+        .toString(this.#size)
+        .padStart(2, '0')
+        .split('')
+        .map((t) => ++t);
+
+      return tile.classList.value === `tile row-${row} col-${col}`;
+    });
+
+    if (allCorrect) {
+      clearInterval(this.#timer);
+      const staticTile = document.querySelector('.tile.static');
+      staticTile.style.visibility = 'visible';
+      this.#playing = false;
+      this.#emptyTile = undefined;
+      this.startButton.innerText = 'Start';
+    }
+  }
+
+  #incrementTime() {
+    this.#time++;
+    const min = String(Math.floor(this.#time / 60)).padStart(2, '0');
+    const sec = String(this.#time % 60).padStart(2, '0');
+    this.timeDisplay.innerText = `${min}:${sec}`;
+  }
+
+  #incrementMoveCount() {
+    this.#moves++;
+    this.moveCount.value = this.#moves;
+  }
+
+  slide({ target: tile }) {
+    if (!this.#playing || !tile.classList.contains('tile')) return;
+
+    const targetTile = {
+      row: +tile.classList.value.match(/row-(\d+)/)[1],
+      col: +tile.classList.value.match(/col-(\d+)/)[1],
+    };
+
+    if (!Game.isAdjacentTile(targetTile, this.#emptyTile)) return;
+
+    tile.classList.value = `tile row-${this.#emptyTile.row} col-${this.#emptyTile.col}`;
+
+    this.#emptyTile = Object.assign(targetTile);
+
+    this.#incrementMoveCount();
+    this.check();
+  }
+}
+
+const GAME = new Game();
