9f7430ad7c2fb489db4f54f2d08f8db5d2edb2c2
Doman <nanodoman@gmail.com>
2026-04-22 22:27:25 +0200

    refactor(game003): transition to es class

--- game003/src/main.js
+++ game003/src/main.js
@@ -1,16 +1,90 @@
-const hexes = document.querySelectorAll('.hex');
+class Hex {
+  constructor(r, c) {
+    this.r = r;
+    this.c = c;
+    this.hit = false;
+    this.target = false;
+  }
 
-window.addEventListener('pointerdown', (e) => {
-  if (e.target.classList.contains('reset')) {
-    for (const hex of hexes) {
-      hex.classList.remove('hit');
+  reset() {
+    this.hit = false;
+    this.target = false;
+  }
+}
+
+class Game {
+  constructor(radius = 2) {
+    this.hexes = new Map();
+    this.grid = document.querySelectorAll('.grid');
+    this.hexNodes = document.querySelectorAll('.hex');
+    this.setupGrid(radius);
+    this.pointerDown = this.pointerDown.bind(this);
+    this.pointerUp = this.pointerUp.bind(this);
+    window.addEventListener('pointerdown', this.pointerDown);
+    window.addEventListener('pointerup', this.pointerUp);
+  }
+
+  pointerUp(e) {
+    e.preventDefault();
+  }
+
+  pointerDown(e) {
+    if (e.target.classList.contains('reset')) {
+      this.resetGrid();
+      return;
+    }
+
+    const hexElement = event.target.closest('.hex');
+    const { r, c } = hexElement.dataset;
+
+    if (!hexElement || this.getHex(r, c).hit) return;
+
+    this.hitHex(r, c);
+
+    e.preventDefault();
+  }
+
+  addHex(r, c) {
+    const key = `${r},${c}`;
+    this.hexes.set(key, new Hex(r, c));
+  }
+
+  getHex(r, c) {
+    const key = `${r},${c}`;
+    return this.hexes.get(key);
+  }
+
+  hitHex(r, c) {
+    this.getHex(r, c).hit = true;
+    this.renderGrid();
+  }
+
+  setupGrid(radius) {
+    for (let r = -radius; r <= radius; r++) {
+      for (let c = -radius; c <= radius; c++) {
+        if (Math.abs(r + c) <= radius) {
+          this.addHex(r, c);
+        }
+      }
     }
   }
 
-  event.target.closest('.hex')?.classList.add('hit');
-  e.preventDefault();
-});
+  resetGrid() {
+    this.hexes.forEach((hex) => {
+      hex.reset();
+    });
 
-window.addEventListener('pointerup', (e) => {
-  e.preventDefault();
-});
+    this.renderGrid();
+  }
+
+  renderGrid() {
+    this.hexNodes.forEach((hexNode) => {
+      const { r, c } = hexNode.dataset;
+      const hex = this.getHex(r, c);
+      hexNode.classList.toggle('hit', hex.hit);
+      hexNode.classList.toggle('target', hex.target);
+    });
+  }
+}
+
+const GAME = new Game();
